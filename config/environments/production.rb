740bf84b8b7cf5e620ed2b8746eac22233ee7ebd
Alexandre Voyer <Alexandre.Voyer@outlook.com>
2015-09-25 14:36:50 +0000

    Set locale to French.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,10 +78,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
-  config.i18n.available_locales = [:en, :fr]
-  config.i18n.default_locale = :fr
-  
 end
 
 ActionMailer::Base.smtp_settings = {
