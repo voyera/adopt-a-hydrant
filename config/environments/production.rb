2c5ec0daa497787d2dc5a7bf0fb3f3dd745fb14d
Alexandre Voyer <Alexandre.Voyer@outlook.com>
2015-09-25 12:25:30 +0000

    Change default locale to fr.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {host: 'adoptahydrant.org'}
+  config.action_mailer.default_url_options = {host: 'gatineau-adopt-hydrant.herokuapp.com'}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -78,6 +78,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.i18n.available_locales = [:en, :fr]
+  config.i18n.default_locale = :fr
+  
 end
 
 ActionMailer::Base.smtp_settings = {
